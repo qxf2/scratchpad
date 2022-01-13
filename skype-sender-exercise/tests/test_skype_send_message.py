1ef922977c0ea5695d7579f12c32e66ffb162ece
Indira Nellutla <indira.nellutla@sciencelogic.com>
2022-01-13 16:05:43 +0530

    added doc string to the code

--- skype-sender-exercise/tests/test_skype_send_message.py
+++ skype-sender-exercise/tests/test_skype_send_message.py
@@ -1,5 +1,5 @@
 """
-Test to send a message to skype channel using "/send-message" endpoint of Skype sender
+Test to send a message to skype channel using "/send-message" endpoint of Qxf2's Skype sender
 
 """
 import json
@@ -10,7 +10,8 @@ from conf import skype_conf as creds
 
 SKYPE_ENDPOINT = 'https://skype-sender.qxf2.com/send-message'
 
-def test_skype_send_message():    
+def test_skype_send_message():   
+    "Send the given message" 
     msg = "Hi, This is a message from Indira"
     data = json.dumps({"msg": msg, "channel": creds.SKYPE_CHANNEL_ID, "API_KEY": creds.API_KEY})    
     headers = {'Content-Type': 'application/json'}  
