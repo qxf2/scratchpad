02fc9fdb66bf543a859ac096c174d508e51e9e32
Qxf2 <mak@qxf2.com>
2022-12-23 22:30:03 +0530

    Got a working example of getting text() from /get_memes

--- rust-api-calls/meme/src/main.rs
+++ rust-api-calls/meme/src/main.rs
@@ -1,3 +1,22 @@
-fn main(){
-    println!("Hello world!");
+/*
+Fetch the top 100 memes from https://imgflip.com/
+Print out the id and the name of the meme
+URL: https://api.imgflip.com/get_memes
+*/
+use std::error::Error;
+use reqwest::Client;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>>{
+    let base_url = "https://api.imgflip.com";
+    let endpoint = "/get_memes";
+    let client = Client::new();
+    let response = client
+                    .get(format!("{}{}",base_url, endpoint))
+                    .send()
+                    .await?
+                    .text()
+                    .await;
+    println!("{:?}", response);
+    Ok(())
 }
