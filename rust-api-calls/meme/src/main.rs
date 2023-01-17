0917579b7bc09ecc355766e98a31360876c84edd
Qxf2 <mak@qxf2.com>
2023-01-17 19:40:29 +0530

    Refactored code within main

--- rust-api-calls/meme/src/main.rs
+++ rust-api-calls/meme/src/main.rs
@@ -3,48 +3,67 @@ Fetch the top 100 memes from https://imgflip.com/
 Print out the id and the name of the meme
 URL: https://api.imgflip.com/get_memes
 */
-use std::error::Error;
-use reqwest::Client;
+use reqwest::{Client, Response};
 use serde::Deserialize;
 
 #[derive(Deserialize, Debug)]
-struct Meme{
+struct Meme {
     id: String,
     name: String,
     url: String,
-    width: i32,
-    height: i32,
-    captions: u8
 }
 
 #[derive(Deserialize, Debug)]
-struct Memes{
-    memes: Vec<Meme>
+struct Memes {
+    memes: Vec<Meme>,
 }
 
 #[derive(Deserialize, Debug)]
-struct GetMemes{
-    success: bool,
-    data: Memes
+struct GetMemes {
+    data: Memes,
+}
+
+async fn http_get(base_url: &str, endpoint: &str) -> Result<Response, reqwest::Error> {
+    let client = Client::new();
+    let response = client.get(format!("{}{}", base_url, endpoint)).send().await;
+    return response;
+}
+
+fn get_empty_memes() -> Memes{
+    Memes{memes: vec![]}
+}
+
+async fn parse_memes_json(value: Response) -> Memes{
+    let get_memes = value.json::<GetMemes>().await;
+    let memes = match get_memes{
+        Ok(val) => val.data,
+        Err(err) => {println!("Rust says {:?}", err);
+                            get_empty_memes()}
+    };
+
+    return memes
+}
+
+async fn get_meme_data() -> Memes {
+    let base_url = "https://api.imgflip.com";
+    let endpoint = "/get_memes";
+    let memes = match http_get(base_url, endpoint).await{
+        Ok(val) => parse_memes_json(val).await,
+        Err(err) => { println!("Rust says {:?}", err);
+                            get_empty_memes()}
+    };
+
+    return memes;
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>>{
-    let base_url = "https://api.imgflip.com";
-    let endpoint = "/get_memes";
-    let client = Client::new();
-    let response = client
-                    .get(format!("{}{}",base_url, endpoint))
-                    .send()
-                    .await?
-                    .json::<GetMemes>()
-                    .await;
-    let all_memes = match response{
-        Ok(val) => val.data,
-        _ => panic!("Error! I did not get a JSON value from /get_memes")
-    };
-    for meme in all_memes.memes{
-        println!("{:?}:{:?}",meme.name,meme.url);
+async fn main() {
+    let all_memes = get_meme_data().await;
+    if all_memes.memes.len() > 0{
+        for meme in all_memes.memes {
+            println!("{},{},{}", meme.name, meme.id, meme.url);
+        }
+    } else {
+        println!("Error when fetching memes. Got zero memes!")
     }
-    Ok(())
 }
