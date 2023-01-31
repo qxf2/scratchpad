e7c07246c6751a4661b22f805155b0196bd6a9a8
Qxf2 <mak@qxf2.com>
2023-01-31 23:41:17 +0530

    Added two tests for showing parameterization

--- rust-api-calls/meme/src/http_methods.rs
+++ rust-api-calls/meme/src/http_methods.rs
@@ -8,3 +8,32 @@ pub async fn http_get(base_url: &str, endpoint: &str) -> Result<Response, reqwes
     let response = client.get(format!("{}{}", base_url, endpoint)).send().await;
     return response;
 }
+
+#[cfg(test)]
+mod tests{
+    use super::*;
+
+    #[tokio::test]
+    async fn test_valid_http_get(){
+        let base_url = "https://qxf2.com";
+        let endpoint = "/qxf2-name";
+        let response = match http_get(base_url, endpoint).await{
+            Ok(_val) => true,
+            Err(_err) => false
+        };
+        assert!(response)
+    }
+
+    #[tokio::test]
+    async fn test_invalid_http_get(){
+        let base_url = "https://blah.qxf2.com";
+        let endpoint = "/this-does-not-exist";
+        let response = match http_get(base_url, endpoint).await{
+            Ok(_val) => true,
+            Err(_err) => false
+        };
+        assert!(!response)        
+    }
+
+
+}
