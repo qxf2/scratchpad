f80b9b0f0fd76871b33f7aa8eec86ec282899e9e
Qxf2 <mak@qxf2.com>
2023-01-31 16:10:03 +0530

    Got a couple of tests working including creating a synthetic response

--- rust-api-calls/meme/src/meme_type.rs
+++ rust-api-calls/meme/src/meme_type.rs
@@ -4,7 +4,6 @@ The meme struct and associated structs and methods
 use serde::Deserialize;
 use reqwest::{Response};
 
-
 #[derive(Deserialize, Debug)]
 pub struct Meme {
     id: String,
@@ -52,4 +51,51 @@ pub async fn parse_memes_json(value: Response) -> Memes{
     };
 
     return memes
+}
+
+#[cfg(test)]
+mod tests{
+    use super::*;
+
+    #[test]
+    fn test_get_empty_memes(){
+        let my_memes = get_empty_memes();
+        assert_eq!(my_memes.length(), 0, "Expected empty Memes, but got {}", my_memes.length())
+    }
+    
+    #[test]
+    fn test_memes_length_one(){
+        let my_memes = Memes{
+            memes: vec![Meme{
+                name:String::from("AJ Styles vs Undertaker"),
+                id: String::from("4096"),
+                url: String::from("https://qxf2.com/wwe")
+            }]
+        };
+        assert_eq!(my_memes.length(), 1, "Expected length to be one but got {}", my_memes.length())
+    }
+
+    #[tokio::test]
+    async fn test_parse_memes_json_error(){
+        use http::response::Builder;
+        let my_response = Builder::new()
+                                .body("Qxf2")
+                                .unwrap();
+        let my_response = Response::from(my_response);
+        let parsed_json = parse_memes_json(my_response).await;
+        
+        assert_eq!(0, parsed_json.length())
+    }
+
+    #[tokio::test]
+    async fn test_parse_memes_json_valid(){
+        use http::response::Builder;
+        let my_response = Builder::new()
+                                .body("{\"data\": {\"memes\":[{\"id\":\"64\", \"name\":\"Surprised Rock\", \"url\":\"https://qxf2.com\"}]}}")
+                                .unwrap();
+        let my_response = Response::from(my_response);
+        let parsed_json = parse_memes_json(my_response).await;
+
+        assert_eq!(1, parsed_json.length())
+    }
 }
