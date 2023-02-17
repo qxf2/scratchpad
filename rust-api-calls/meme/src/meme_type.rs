ffe8b3ebbbbd5da4b3dd99501bf106a54a3b60b8
Shivahari, P <shivaharip@sciencelogic.com>
2023-02-17 15:02:15 +0530

    Added steps to serialize a struct, added an example snippet on how to add a trait to the structs

--- rust-api-calls/meme/src/meme_type.rs
+++ rust-api-calls/meme/src/meme_type.rs
@@ -1,24 +1,34 @@
 /*
 The meme struct and associated structs and methods
 */
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
+// from serde import Deserilize, Serialize
 use reqwest::{Response};
 
-#[derive(Deserialize, Debug)]
+trait Content<T> {
+    fn new(&self) -> T {
+    }
+}
+
+#[derive(Deserialize, Serialize, Debug)]
 pub struct Meme {
     id: String,
     name: String,
     url: String,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Serialize, Debug)]
 pub struct Memes {
     memes: Vec<Meme>,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Serialize, Debug)]
 pub struct GetMemes {
     pub data: Memes,
+}
+
+impl GetMemes{
+    fn new
 }
 
 impl Meme{
@@ -56,6 +66,7 @@ pub async fn parse_memes_json(value: Response) -> Memes{
 #[cfg(test)]
 mod tests{
     use super::*;
+    use serde_json;
 
     #[test]
     fn test_get_empty_memes(){
@@ -90,12 +101,72 @@ mod tests{
     #[tokio::test]
     async fn test_parse_memes_json_valid(){
         use http::response::Builder;
+        let meme = Meme{
+            id: String::from("64"),
+            name: String::from("Surprised Rock"),
+            url: String::from("https://qxf2.com")
+        }; 
+        let memes: Memes = Memes{
+            memes: vec!(meme)
+        };
+        let json_data: GetMemes = GetMemes{
+            data: memes
+         };
+ 
+        
+        // Serialize the GetMemes struct
+        let meme_data_json = serde_json::to_string(&json_data).unwrap();
+        println!("The Meme Data JSON is {:?}", meme_data_json);
+    
         let my_response = Builder::new()
-                                .body("{\"data\": {\"memes\":[{\"id\":\"64\", \"name\":\"Surprised Rock\", \"url\":\"https://qxf2.com\"}]}}")
+                                .body(meme_data_json)
                                 .unwrap();
         let my_response = Response::from(my_response);
         let parsed_json = parse_memes_json(my_response).await;
 
         assert_eq!(1, parsed_json.length())
     }
+    }
+
+// Add a trait for the structs defined based on the following example
+/*
+pub trait Content {
+    fn get_test_data() -> Self;
 }
+
+#[derive(Debug)]
+pub struct Meme {
+    pub id: String,
+    pub name: String,
+    pub url: String,
+}
+
+#[derive(Debug)]
+pub struct Memes {
+    memes: Vec<Meme>,
+}
+
+
+impl Content for Meme{
+    fn get_test_data() -> Meme{
+        return Meme{id: String::from("1"),
+                    name: String::from("Qxf2"),
+                    url: String::from("https://qxf2.com")}
+    }
+}
+
+impl Content for Memes{
+    fn get_test_data() -> Memes{
+        return Memes{
+            memes: vec!(Meme::get_test_data())
+        }
+    }
+}
+
+fn main(){
+    let test_meme_data = Meme::get_test_data();
+    println!("The Meme is {:?}", test_meme_data);
+    let memes = Memes::get_test_data();
+    println!("The Meme is {:?}", memes);
+}
+ */
