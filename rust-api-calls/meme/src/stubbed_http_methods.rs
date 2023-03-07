44e6495d74185ced1504023bd78b4b721c006ce4
Shivahari P <shivahari@qxf2.com>
2023-03-07 14:43:44 +0530

    Undid my last change to prevent warning

--- rust-api-calls/meme/src/stubbed_http_methods.rs
+++ rust-api-calls/meme/src/stubbed_http_methods.rs
@@ -4,8 +4,7 @@ A stubbed library for http methods
 
 use reqwest::{Client, Response};
 
-pub async fn http_get(endpoint: &str) -> Result<Response, reqwest::Error> {
-    let base_url = "https://not-there.qxf2.com";
+pub async fn http_get(base_url: &str, endpoint: &str) -> Result<Response, reqwest::Error> {
     let client = Client::new();
     let response = client.get(format!("{}{}", base_url, endpoint)).send().await;
     return response;
