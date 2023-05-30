484c6c1990a9307be746b45649e91dd40577c2ca
ajitava <ajitava.deb@qxf2.com>
2023-05-30 20:11:50 +0530

    Deleted .vscode and included tests under mod

--- rust-csv-crud/src/main.rs
+++ rust-csv-crud/src/main.rs
@@ -31,7 +31,7 @@ fn get_cyborgs() -> Vec<Cyborg> {
             model: "2IO".to_string(),
             organization: "Skynet".to_string(),
             abilities: "manner".to_string(),
-            creation_date: NaiveDate::from_ymd_opt(2015, 4, 3),
+            creation_date: NaiveDate::from_ymd_opt(123456789, 4, 3),
         },        
 
     ]
@@ -55,7 +55,7 @@ fn write_csv_details(csv_path: &str) -> Result<(), Box<dyn Error>> {
             cyborg.model,
             cyborg.organization,
             cyborg.abilities,
-            cyborg.creation_date.map(|date| date.to_string()).unwrap_or_else(|| "1500, 4, 5".to_string()),
+            cyborg.creation_date.map(|date| date.to_string()).unwrap_or_else(|| "1800-05-04".to_string()),
         ]) {
             Ok(record_result) => record_result,
             Err(err) => return Err(Box::new(err)),            
@@ -106,94 +106,93 @@ fn read_csv_details(
 #[cfg(test)]
 mod tests {
     use super::*;
-}
 
-#[test]
-fn test_read_empty_csv_file() {
-    let csv_path = "empty.csv";
-    let result = read_csv_details(csv_path, true, true);
-    assert!(result.is_ok(), "Failed to read CSV file");
-
-    let csv_data = result.unwrap();
-    assert!(csv_data.is_empty(), "CSV data is expected to be empty, but found records in {} rows", csv_data.len());
-}
-
-#[test]
-fn test_csv_column_count() {
-    let csv_path = "cyborg.csv";
-    let result = read_csv_details(csv_path, true,true);
-    assert!(result.is_ok(), "Failed to read CSV file");
-
-    let csv_data = result.unwrap();
-    let expected_column_count = 3;
-
-    for (row_count, row) in csv_data.iter().enumerate() {
-        assert_eq!(
-            row.len(),
-            expected_column_count,
-            "Unexpected number of columns in row {}.Expected: {}, Actual: {}",
-            row_count + 1,
-            expected_column_count,
-            row.len()
-        );
+    #[test]
+    fn test_read_empty_csv_file() {
+        let csv_path = "empty.csv";
+        let result = read_csv_details(csv_path, true, true);
+        assert!(result.is_ok(), "Failed to read CSV file");
+    
+        let csv_data = result.unwrap();
+        assert!(csv_data.is_empty(), "CSV data is expected to be empty, but found records in {} rows", csv_data.len());
     }
-}
-
-#[test]
-fn test_invalid_csv_file() {
-    let csv_path = "invalid.csv";
-    let result = read_csv_details(csv_path, false,true);
-    assert!(result.is_ok(), "An error occured while reading the CSV file");
-}
-
-#[test]
-fn test_csv_data_contains_special_characters() {
-    let csv_path = "cyborg.csv";
-    let result = read_csv_details(csv_path, true,true).unwrap();
-
-    let special_characters = "!@$%^&*()_+=[]{}|<>?";
-
-    for (row_index, row) in result.iter().enumerate() {
-        for (column_index, cell) in row.iter().enumerate() {
-            assert!(
-                !cell.chars().any(|c| special_characters.contains(c)),
-                "Special character found in CSV data at row {}, column {}",
-                row_index + 1,
-                column_index + 1
+    
+    #[test]
+    fn test_csv_column_count() {
+        let csv_path = "cyborg.csv";
+        let result = read_csv_details(csv_path, true,true);
+        assert!(result.is_ok(), "Failed to read CSV file");
+    
+        let csv_data = result.unwrap();
+        let expected_column_count = 3;
+    
+        for (row_count, row) in csv_data.iter().enumerate() {
+            assert_eq!(
+                row.len(),
+                expected_column_count,
+                "Unexpected number of columns in row {}.Expected: {}, Actual: {}",
+                row_count + 1,
+                expected_column_count,
+                row.len()
             );
         }
     }
-}
-
-#[test]
-fn test_read_large_csv_file() {
-    let csv_path = "large.csv";
-    let result = read_csv_details(csv_path, true, true);
-
-    let csv_data = result.unwrap();
-    let expected_row_count = 10000;
-    let expected_column_count = 15;
-
-    assert_eq!(
-        csv_data.len(),
-        expected_row_count,
-        "Unexpected number of rows in CSV. Expected: {}, Actual: {}",
-        expected_row_count,
-        csv_data.len()
-    );
-
-    for (row_count, row) in csv_data.iter().enumerate() {
-        assert_eq!(
-            row.len(),
-            expected_column_count,
-            "Unexpected number of columns in row {}. Expected: {}, Actual: {}",
-            row_count + 1,
-            expected_column_count,
-            row.len()
-        );
+    
+    #[test]
+    fn test_invalid_csv_file() {
+        let csv_path = "invalid.csv";
+        let result = read_csv_details(csv_path, false,true);
+        assert!(result.is_ok(), "An error occured while reading the CSV file");
     }
+    
+    #[test]
+    fn test_csv_data_contains_special_characters() {
+        let csv_path = "cyborg.csv";
+        let result = read_csv_details(csv_path, true,true).unwrap();
+    
+        let special_characters = "!@$%^&*()_+=[]{}|<>?";
+    
+        for (row_index, row) in result.iter().enumerate() {
+            for (column_index, cell) in row.iter().enumerate() {
+                assert!(
+                    !cell.chars().any(|c| special_characters.contains(c)),
+                    "Special character found in CSV data at row {}, column {}",
+                    row_index + 1,
+                    column_index + 1
+                );
+            }
+        }
+    }
+    
+    #[test]
+    fn test_read_large_csv_file() {
+        let csv_path = "large.csv";
+        let result = read_csv_details(csv_path, true, true);
+    
+        let csv_data = result.unwrap();
+        let expected_row_count = 10000;
+        let expected_column_count = 15;
+    
+        assert_eq!(
+            csv_data.len(),
+            expected_row_count,
+            "Unexpected number of rows in CSV. Expected: {}, Actual: {}",
+            expected_row_count,
+            csv_data.len()
+        );
+    
+        for (row_count, row) in csv_data.iter().enumerate() {
+            assert_eq!(
+                row.len(),
+                expected_column_count,
+                "Unexpected number of columns in row {}. Expected: {}, Actual: {}",
+                row_count + 1,
+                expected_column_count,
+                row.len()
+            );
+        }
+    }    
 }
-
 
 fn main() {
     let result = write_csv_details("write_file.csv");
